975859dba0f5a4061a44ef8e4aca96971b61794c
Elena Dragunova <elena.dragunova@saritasa.com>
2020-01-08 10:28:16 +0500

    first commit

---
+++ src/components/ToDo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>ToDo</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+})
+
+export default class ToDo extends Vue {}
+</script>
+
+<style lang="scss">
+
+</style>
