80ba515f9e7f9187db133750f59c008ed75a54d9
elena-dragunova <elenak-8647@gmail.com>
2020-01-08 11:43:27 +0500

    Create form for adding tasks.

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,18 +1,66 @@
 <template>
-    <div>
-        <h1>ToDo</h1>
+    <div class="to-do">
+        <h1>ToDo App</h1>
+        <ToDoForm @taskAdded="addTask" button-text="Create task"/>
+        <ul class="tasks-list">
+            <li v-for="task in tasks">
+                <input type="checkbox" :checked="task.completed">
+                <p>{{ task.description }}</p>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import ToDoForm from '@/components/ToDoForm.vue';
+import Task from '@/types/Task.ts';
 
 @Component({
+    components: {
+        ToDoForm,
+    },
 })
 
-export default class ToDo extends Vue {}
+export default class ToDo extends Vue {
+    public tasks: Task[] = [
+        { description: 'Make Coffee', completed: true },
+        { description: 'Feed Dragons', completed: false },
+    ];
+
+    public addTask(description: string): void {
+        this.tasks.push({ description, completed: false });
+    };
+}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.to-do {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
 
+.tasks-list {
+    list-style: none;
+    padding-left: 0;
+    margin-bottom: 0;
+
+    li {
+        margin-bottom: 4px;
+        display: flex;
+        align-items: center;
+
+        input {
+            width: 20px;
+            height: 20px;
+            margin-right: 10px;
+        }
+
+        p {
+            margin: 0;
+            font-size: 18px;
+        }
+    }
+}
 </style>
---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <form @submit.prevent="emitTask">
+        <input type="text" placeholder="Enter task..." v-model="task">
+        <button type="submit">{{ buttonText }}</button>
+    </form>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class ToDoForm extends Vue {
+
+    @Prop({ type: String, default: 'Add task' }) readonly buttonText!: string;
+
+    public task: string = '';
+
+    public emitTask(): void {
+        this.$emit('taskAdded', this.task);
+        this.task = '';
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
